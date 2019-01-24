28bdd22121a7ef9ed1b0043130b9c89f3799c7e2
komal3770 <komal3770@gmail.com>
2019-01-24 11:37:02 +0530

    Weather app changes

--- src/main/resources/dbScripts.sql
+++ src/main/resources/dbScripts.sql
@@ -10,6 +10,6 @@ insert into city(city,state_id) values
 insert into role(role_name) values ('USER');
 
 insert into weather_data(date,description,tempreture,city_id)
-values('2019-01-23','Partly Cloudy',23.3,1),
-('2019-01-24','Partly Cloudy',26,1),
-('2019-01-25','Breezy',20,1);
+values('2019-01-24','Partly Cloudy',23.3,1),
+('2019-01-25','Partly Cloudy',26,1),
+('2019-01-26','Breezy',20,1);
